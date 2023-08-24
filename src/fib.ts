e500254d0040f9a7afdf811d63bcb2e84bf71fb4
aishaalsubaie <asubaie@andrew.cmu.edu>
2023-08-24 22:49:47 +0300

    deffinal

--- src/fib.ts
+++ src/fib.ts
@@ -1,11 +1,6 @@
 // util function that computes the fibonacci numbers
-<<<<<<< HEAD
 export function fibonacci(nu:number) :number {
   if (nu < 0) {
-=======
-export function fibonacci(n:number) :number {
-  if (n < 0) {
->>>>>>> main
     return -1;
   } else if (nu == 0) {
     return 0;
@@ -13,10 +8,5 @@ export function fibonacci(n:number) :number {
     return 1;
   }
 
-<<<<<<< HEAD
   return fibonacci(nu - 1) + fibonacci(nu - 2);
 }
-=======
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
->>>>>>> main
